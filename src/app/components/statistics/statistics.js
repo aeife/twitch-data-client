bbbfea280b95934f1ec912eb0e4ec78de7b56801
André Eife <andre.eife@gmail.com>
2015-07-30 19:16:53 +0200

    clean up

--- src/app/components/statistics/statistics.js
+++ src/app/components/statistics/statistics.js
@@ -2,154 +2,152 @@
 
 angular.module('twitchdata.components.statistics', [])
   .provider('statisticsService', function () {
+    this.$get = function () {
+      var monthDiff = function (d1, d2) {
+        return d2.getMonth() - d1.getMonth() + (12 * (d2.getFullYear() - d1.getFullYear()));
+      };
 
-    var monthDiff = function (d1, d2) {
-      return d2.getMonth() - d1.getMonth() + (12 * (d2.getFullYear() - d1.getFullYear()));
-    };
+      var addMissing = function (stats, attrs) {
+        var attrsObject = {};
+        attrs.forEach(function (attr) {
+          attrsObject[attr] = 0;
+        });
 
-    var addMissing = function (stats, attrs) {
-      var attrsObject = {};
-      attrs.forEach(function (attr) {
-        attrsObject[attr] = 0;
-      });
+        var tmpStats = stats.slice();
+        // floor hours
+        tmpStats.map(function (stat) {
+          stat.date.setMinutes(0);
+          return stat;
+        });
 
-      var tmpStats = stats.slice();
-      // floor hours
-      tmpStats.map(function (stat) {
-        stat.date.setMinutes(0);
-        return stat;
-      });
+        // precition frame dates
+        var lastDate = new Date(_.last(stats).date);
+        var precisionBorders = statisticsService.getPrecisionBorders(lastDate);
+        var firstDate = new Date(_.first(stats).date);
 
-      // precition frame dates
-      var lastDate = new Date(_.last(stats).date);
-      var lastMonth = new Date(lastDate.getTime());
-      lastMonth = new Date(lastMonth.setMonth(lastMonth.getMonth() - 1));
-      var lastQuarter = new Date(lastDate.getTime());
-      lastQuarter = new Date(lastQuarter.setMonth(lastQuarter.getMonth() -3));
-      var firstDate = new Date(_.first(stats).date);
+        // generate all expected stats entries
+        var expectedStats = {};
+        var d;
 
-      // generate all expected stats entries
-      var expectedStats = {};
-      var d;
-
-      var hourCount = Math.floor((lastDate - lastMonth) / 36e5) + 1;
-      for (var i = 0; i < hourCount; i++) {
-        d = new Date(lastDate);
-        d.setHours(d.getHours() - i);
-        if (d.getTime() < firstDate.getTime()) {
-          break;
+        var hourCount = Math.floor((lastDate - precisionBorders.hourly) / 36e5) + 1;
+        for (var i = 0; i < hourCount; i++) {
+          d = new Date(lastDate);
+          d.setHours(d.getHours() - i);
+          if (d.getTime() < firstDate.getTime()) {
+            break;
+          }
+          expectedStats[d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate() + '-' + d.getHours()] = _.extend({
+            date: d,
+            hour: d.getUTCHours(),
+            day: d.getUTCDate(),
+            month: d.getUTCMonth()+1,
+            year: d.getFullYear()
+          }, attrsObject);
         }
-        expectedStats[d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate() + '-' + d.getHours()] = _.extend({
-          date: d,
-          hour: d.getUTCHours(),
-          day: d.getUTCDate(),
-          month: d.getUTCMonth()+1,
-          year: d.getFullYear()
-        }, attrsObject);
-      }
 
-      var dayCount = Math.floor((lastMonth - lastQuarter) / 864e5) + 1;
-      for (var j = 0; j < dayCount; j++) {
-        d = new Date(lastMonth);
-        d.setDate(d.getDate() - j);
-        if (d.getTime() < firstDate.getTime()) {
-          break;
+        var dayCount = Math.floor((precisionBorders.hourly - precisionBorders.daily) / 864e5) + 1;
+        for (var j = 0; j < dayCount; j++) {
+          d = new Date(precisionBorders.hourly);
+          d.setDate(d.getDate() - j);
+          if (d.getTime() < firstDate.getTime()) {
+            break;
+          }
+          expectedStats[d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate()] = _.extend({
+            date: d,
+            day: d.getUTCDate(),
+            month: d.getUTCMonth()+1,
+            year: d.getFullYear()
+          }, attrsObject);
         }
-        expectedStats[d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate()] = _.extend({
-          date: d,
-          day: d.getUTCDate(),
-          month: d.getUTCMonth()+1,
-          year: d.getFullYear()
-        }, attrsObject);
-      }
 
-      var monthCount = monthDiff(firstDate, lastQuarter) + 1;
-      for (var k = 0; k < monthCount; k++) {
-        d = new Date(lastQuarter);
-        d.setMonth(d.getMonth()- k);
-        if (d.getTime() < firstDate.getTime()) {
-          break;
+        var monthCount = monthDiff(firstDate, precisionBorders.daily) + 1;
+        for (var k = 0; k < monthCount; k++) {
+          d = new Date(precisionBorders.daily);
+          d.setMonth(d.getMonth()- k);
+          if (d.getTime() < firstDate.getTime()) {
+            break;
+          }
+          expectedStats[d.getFullYear() + '-' + d.getMonth()] =  _.extend({
+            date: d,
+            month: d.getUTCMonth()+1,
+            year: d.getFullYear()
+          }, attrsObject);
         }
-        expectedStats[d.getFullYear() + '-' + d.getMonth()] =  _.extend({
-          date: d,
-          month: d.getUTCMonth()+1,
-          year: d.getFullYear()
-        }, attrsObject);
-      }
 
-      // overwrite expected with real stats where entries are available
-      tmpStats.forEach(function (stat) {
-        var key;
-        if (typeof stat.hour !== 'undefined') {
-          key = stat.date.getFullYear() + '-' + stat.date.getMonth() + '-' + stat.date.getDate() + '-' + stat.date.getHours();
-        } else if (typeof stat.day !== 'undefined') {
-          key = stat.date.getFullYear() + '-' + stat.date.getMonth() + '-' + stat.date.getDate();
-        } else {
-          key = stat.date.getFullYear() + '-' + stat.date.getMonth();
+        // overwrite expected with real stats where entries are available
+        tmpStats.forEach(function (stat) {
+          var key;
+          if (typeof stat.hour !== 'undefined') {
+            key = stat.date.getFullYear() + '-' + stat.date.getMonth() + '-' + stat.date.getDate() + '-' + stat.date.getHours();
+          } else if (typeof stat.day !== 'undefined') {
+            key = stat.date.getFullYear() + '-' + stat.date.getMonth() + '-' + stat.date.getDate();
+            stat.date.setHours(1);
+          } else {
+            key = stat.date.getFullYear() + '-' + stat.date.getMonth();
+            stat.date.setDate(1);
+          }
+          expectedStats[key] = stat;
+        });
+
+        var result = [];
+        for (var stat in expectedStats) {
+          result.push(expectedStats[stat]);
         }
-        expectedStats[key] = stat;
-      });
+        return result.reverse();
+      };
 
-      var result = [];
-      for (var stat in expectedStats) {
-        result.push(expectedStats[stat]);
-      }
-      return result.reverse();
-    };
+      var getAvgForTimeFrame = function (stats, attrs, timeframe, limit, offset) {
+        var tmpStats = stats.slice();
+        tmpStats.reverse();
 
-    var getAvgForTimeFrame = function (stats, attrs, timeframe, limit, offset) {
-      var tmpStats = stats.slice();
-      tmpStats.reverse();
+        var timeFrames =  _.groupBy(tmpStats, function (stat) {
+          var group = stat.year;
+          if (timeframe === 'month') {
+            group += '-' + stat.month;
+          } else if (timeframe === 'day') {
+            group += '-' + stat.month + '-' + stat.day;
+          }
+          return group;
+        });
+        var timeFrameKeys = Object.keys(timeFrames).sort();
+        timeFrameKeys.reverse();
 
-      var timeFrames =  _.groupBy(tmpStats, function (stat) {
-        var group = stat.year;
-        if (timeframe === 'month') {
-          group += '-' + stat.month;
-        } else if (timeframe === 'day') {
-          group += '-' + stat.month + '-' + stat.day;
-        }
-        return group;
-      });
-      var timeFrameKeys = Object.keys(timeFrames).sort();
-      timeFrameKeys.reverse();
+        var count = 0;
+        var result = {};
+        attrs.forEach(function (attr) {
+          result[attr] = 0;
+        });
 
-      var count = 0;
-      var result = {};
-      attrs.forEach(function (attr) {
-        result[attr] = 0;
-      });
-
-      for (var i = 0; i < limit; i++) {
-        if (timeFrames[timeFrameKeys[i + offset]]) {
-          timeFrames[timeFrameKeys[i+offset]].forEach(function (stat) {
-            attrs.forEach(function (attr) {
-              result[attr] += stat[attr];
+        for (var i = 0; i < limit; i++) {
+          if (timeFrames[timeFrameKeys[i + offset]]) {
+            timeFrames[timeFrameKeys[i+offset]].forEach(function (stat) {
+              attrs.forEach(function (attr) {
+                result[attr] += stat[attr];
+              });
+              count++;
             });
-            count++;
+          }
+        }
+
+        if (count > 0) {
+          attrs.forEach(function (attr) {
+            result[attr] = result[attr] / count;
           });
         }
-      }
 
-      if (count > 0) {
+        return result;
+      };
+
+      var calculateGrowth = function (first, second, attrs) {
+        var result = {};
+
         attrs.forEach(function (attr) {
-          result[attr] = result[attr] / count;
+          result[attr] = second[attr] ? (first[attr] / second[attr]) * 100 - 100 : null;
         });
-      }
 
-      return result;
-    };
+        return result;
+      };
 
-    var calculateGrowth = function (first, second, attrs) {
-      var result = {};
-
-      attrs.forEach(function (attr) {
-        result[attr] = second[attr] ? (first[attr] / second[attr]) * 100 - 100 : null;
-      });
-
-      return result;
-    };
-
-    this.$get = function () {
       var statisticsService = {
         // adds missing collection run entries to stats array
         addMissingCollectionRunsToGame: function (stats, lastCollectionRun, attrs) {
@@ -262,7 +260,21 @@ angular.module('twitchdata.components.statistics', [])
           });
 
           return peak;
-        }
+        },
+        getPrecisionBorders: function (lastDate) {
+          var lastMonth = new Date(lastDate.getTime());
+          lastMonth = new Date(lastMonth.setMonth(lastMonth.getMonth() - 1));
+          var lastQuarter = new Date(lastDate.getTime());
+          lastQuarter = new Date(lastQuarter.setMonth(lastQuarter.getMonth() -3));
+
+          return {
+            hourly: lastMonth,
+            daily: lastQuarter
+          };
+        },
+        monthNames: ["January", "February", "March", "April", "May", "June",
+          "July", "August", "September", "October", "November", "December"
+        ]
       };
 
       return statisticsService;
